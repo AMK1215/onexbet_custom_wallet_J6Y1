f15dd4ba505895062c299f0dc2e038189fff09e2
AMK1215 <aungmyokhaing1215@gmail.com>
2025-10-04 13:52:27 +0630

    custom wallet integrate

---
+++ app/Console/Kernel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console;
+
+use App\Console\Commands\FetchWagers;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     */
+    protected $commands = [
+        FetchWagers::class,
+    ];
+
+    protected function schedule(Schedule $schedule): void
+    {
+        $schedule->command('wagers:fetch')->everyFiveMinutes();
+        $schedule->command('app:clean-up-old-place-bet')->monthlyOn(3, '23:59');
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
